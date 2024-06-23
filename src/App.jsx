39077d39e1042e691ae691a2b797085bad7bfbc1
Devendra kumar <devendrakumar9580387090@gamil.com>
2024-06-23 11:04:30 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
-import React from 'react'
+import React, { useContext } from 'react'
 import Sidebar from './componets/Sidebar'
 import Player from './componets/Player'
 import Display from './componets/Display'
+import { PlayerContext } from './context/PlayerContext'
 
 const App = () => {
+  const {audioRef,track} = useContext(PlayerContext)
   return (
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
@@ -12,6 +14,7 @@ const App = () => {
         <Display />
       </div>
       <Player />
+      <audio ref={audioRef}  src={track.file} preload='auto'></audio>
 
     </div>
   )
